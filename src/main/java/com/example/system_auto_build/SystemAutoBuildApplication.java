ac67dc76ac9adc86037d89670e0481e1d65e3247
ashish <ashishmahanta121@gmail.com>
2025-09-12 22:43:07 +0530

    first commit

---
+++ src/main/java/com/example/system_auto_build/SystemAutoBuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.system_auto_build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemAutoBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemAutoBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/system_auto_build/SystemAutoBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.system_auto_build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemAutoBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

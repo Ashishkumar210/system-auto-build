99893a97e4f508a43ba4fa836f81094d116d2297
ashish <ashishmahanta121@gmail.com>
2026-03-22 19:22:49 +0530

    changesw

--- src/main/java/com/example/system_auto_build/controller/EmployeeController.java
+++ src/main/java/com/example/system_auto_build/controller/EmployeeController.java
@@ -25,7 +25,7 @@ public class EmployeeController {
 
     @GetMapping("/gets")
     public String getDetai(){
-        return "checking from Awrssggg";
+        return "checking from Awrssg";
     }
 
 }
